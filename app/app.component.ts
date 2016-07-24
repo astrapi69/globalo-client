c498e02407466f7377e6948fb8f4663da8317f1f
Asterios Raptis <asterios.raptis@gmx.net>
2016-07-24 15:17:43 +0200

    initial version of the globalo-client

---
+++ app/app.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'globalo-app',
+  template: '<h1>The new globalo app build with Angular 2</h1>'
+})
+export class AppComponent { }
